d41d707c92cc82ca5c257fe74505e7139f8f7c77
Renan Castro <renan9008@gmail.com>
2024-08-09 14:15:07 -0300

    .

--- src/app/api/funcionarioCreation/route.ts
+++ src/app/api/funcionarioCreation/route.ts
@@ -1,11 +1,17 @@
 import { ResponseType } from "axios";
 import { NextResponse} from "next/server";
-import Funcionarios from "@/app/models/funcionario";
+import { PrismaClient, Prisma } from '@prisma/client';
+const prisma = new PrismaClient()
 export async function POST(request : Request)  : Promise<void | Response>  {
     try{
         const {name, cpf, email} = await request.json();
 
-        const createdFuncionario = await Funcionarios.create({name:name, cpf: cpf, email: email})
+        const createdFuncionario = await prisma.funcionarios.create({
+            data:{ 
+                name,
+                cpf,
+                email},
+            })
         return NextResponse.json(
             {
                 response: `Funcionario created: ${createdFuncionario}`
