b37e5030febea0418f546db8f87c99f0d4f8a091
Renan Castro <renan9008@gmail.com>
2024-08-09 14:36:05 -0300

    bug fix

--- src/app/axios/handler.ts
+++ src/app/axios/handler.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export const handler = axios.create(
     {
-        baseURL: "https://external-site-integration-with-salesforce-teams-7naoseylf.vercel.app/api",
+        baseURL: "external-site-integration-with-salesforce-tea-git-bc6c3b-cuecas.vercel.app/api",
         headers: {
             "Content-Type" : "application/json"
         }
