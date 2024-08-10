35f6953fabac3baadd188610389aff67afb25a98
Victor Sales <thehoodgamer20@gmail.com>
2024-08-10 18:30:25 -0300

    atualizado a requisição get

--- src/app/api/funcionarioCreation/route.ts
+++ src/app/api/funcionarioCreation/route.ts
@@ -2,22 +2,24 @@ import { ResponseType } from "axios";
 import { NextResponse} from "next/server";
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
-export async function POST(request : Request)  : Promise<void | Response>  {
-    try{
-        const {name, cpf, email} = await request.json();
-
-        const createdFuncionario = await prisma.funcionarios.create({
-            data:{ name, cpf, email}, 
-        })
-        return NextResponse.json(
-            {
-                response: `Funcionario created: ${createdFuncionario}`
-            }
-        )
-    }catch(error)
-    {
-        return NextResponse.json({
-            response: error
-        })
+export async function GET(request: Request): Promise<void | Response> {
+    try {
+      const json = await request.json();
+      const { cpf } = json;
+  
+      const funcionarioReturned = await prisma.funcionarios.findFirst({
+        where: {
+          cpf: cpf,
+        },
+      });
+  
+      return NextResponse.json({
+        response: funcionarioReturned,
+      });
+    } catch (error) {
+      return NextResponse.json({
+        response: `Error: ${error}`,
+      });
     }
-}
+  }
+  
